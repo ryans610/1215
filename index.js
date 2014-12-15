ab713d47c67d18c564370e27bae80957ec24a165
ryans610 <ryans610@gmail.com>
2014-12-15 20:14:58 +0800

    bug fix

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 window.onload=function(){
 	alert("鴨鴨");
 	﻿console.log("呱");
+	var alist=document.getElementsByTagName("li");
+	for(var i=0;i<alist.length;i++){
+		alist.item(i).style.display="none";
+	}
 	document.getElementById("cover").addEventListener("mouseover",function(){
 		var alist=document.getElementsByTagName("li");
 		for(var i=0;i<alist.length;i++){
-			alist.item(i).style.display="block";
+			alist.item(i).style.display="inline";
+		}
+	});
+	document.getElementById("cover").addEventListener("mouseout",function(){
+		var alist=document.getElementsByTagName("li");
+		for(var i=0;i<alist.length;i++){
+			alist.item(i).style.display="none";
 		}
 	});
 };
